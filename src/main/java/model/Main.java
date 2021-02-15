c2caadacc33c250ee1c2e2ea9c3c02c201fae8a3
AlexandrSid <alexandr.sidorkin@gmail.com>
2021-02-15 04:14:57 +0300

    transfer

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -4,8 +4,10 @@ import com.google.gson.Gson;
 import repository.ArticlesRepository;
 import repository.InMemoryArticlesRepository;
 
+import java.sql.*;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.ResourceBundle;
 
 public class Main {
     //не уверен, что данный класс лучше всего подходит для этого,
@@ -39,15 +41,39 @@ public class Main {
 //        Main.initializeRepo(repository);
 //        repository.getAll().stream().map(Article::toString).forEach(System.out::println);
 
-        String[] arrayOfNumbers = {"one", "two", "three", "four", "five"};
-        String json = new Gson().toJson(arrayOfNumbers);
-        System.out.println(json);
+//        String[] arrayOfNumbers = {"one", "two", "three", "four", "five"};
+//        String json = new Gson().toJson(arrayOfNumbers);
+//        System.out.println(json);
+//
+//        String tags = "[Word1,Word2,words 3,Words 4]";
+//        String[] tagsStringsArray = new Gson().fromJson(json, String[].class);
+//        Arrays.stream(tagsStringsArray).forEach(System.out::println);
 
-        String tags = "[Word1,Word2,words 3,Words 4]";
-        String[] tagsStringsArray = new Gson().fromJson(json, String[].class);
-        Arrays.stream(tagsStringsArray).forEach(System.out::println);
+        try {
+            playSQL();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/wikime_test";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "admin";
+
+    public static void playSQL() throws SQLException {
+//        ResourceBundle resources = ResourceBundle.getBundle("src/main/resources/db/database.properties");
+//        String url = resources.getString("database.url");
+//        String username = resources.getString("database.username");
+//        String password = resources.getString("database.password");
+
+        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        Statement statement = connection.createStatement();
 
 
+        statement.close();
+        connection.close();
     }
 
 
---
+++ src/main/resources/db/script.sql
@@ -0,0 +1,5 @@
+-- create database wikime_test;
+create table tags(
+                     id serial not null primary key,
+                     tag_body varchar(64)
+);
